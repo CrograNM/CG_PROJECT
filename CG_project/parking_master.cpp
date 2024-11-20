ce7d685bd3a2a205dc8450d905be7a264ff0d1c9
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-11-20 21:33:56 +0900

    바퀴, 헤드라이트

--- CG_project/parking_master.cpp
+++ CG_project/parking_master.cpp
@@ -52,12 +52,12 @@ void initBox();
 GLfloat Box[14 * 3][3];
 GLfloat	Box_Color[14 * 3][3];
 
-//Block 그리기 - 랜덤 위치에 3종류
+//Block 그리기 - 차체
 void initBlock();
 #define BLOCK_SIZE 0.5f
-#define WHEEL_SIZE BLOCK_SIZE / 2
-GLfloat Block[3][12 * 3][3];
-GLfloat	Block_Color[3][12 * 3][3];
+#define WHEEL_SIZE BLOCK_SIZE / 4
+GLfloat Block[4][12 * 3][3];
+GLfloat	Block_Color[4][12 * 3][3];
 
 // 크레인 그리기 - vao 2
 GLfloat	figure[MAX_FIGURE][TRI_COUNT * 3][3];	//0: 몸통, 1: 중간몸통, 2,3: 위팔, 4,5: 포신
@@ -126,8 +126,51 @@ glm::mat4 SRT_MATRIX()
 
 	return Ry1;
 }
-// 바퀴를 원점으로 이동시키는 함수 + 기본적으로 90도 회전시킴
-glm::mat4 Wheel_on_000(int type)
+
+// 자동체 몸체의 변환, 이 함수를 기준으로 헤드라이트, 바퀴 등의 위치가 정해진다.
+
+// 헤드라이트를 차량의 앞으로 고정
+glm::mat4 Headlights(int left_right) //num은 4개 바퀴의 번호, type은 실린더, 뚜껑 객체 종류
+{
+	glm::mat4 T = glm::mat4(1.0f);
+	glm::mat4 Ry = glm::mat4(1.0f);
+	//glm::mat4 S = glm::mat4(1.0f);
+
+	if (left_right == 0)
+	{
+		T = glm::translate(T, glm::vec3(-BLOCK_SIZE/3, BLOCK_SIZE / 8, BLOCK_SIZE));
+	}
+	else if(left_right == 1)
+	{
+		T = glm::translate(T, glm::vec3(BLOCK_SIZE/3, BLOCK_SIZE / 8, BLOCK_SIZE));
+	}
+	
+	return T;
+}
+
+// 바퀴
+glm::mat4 Wheels(int num)
+{
+	glm::mat4 T2 = glm::mat4(1.0f);
+	if (num == 1) //앞 기준 왼쪽 앞
+	{
+		T2 = glm::translate(T2, glm::vec3(-(BLOCK_SIZE / 2 + WHEEL_SIZE / 4), 0.0f, BLOCK_SIZE * 0.5f));
+	}
+	if (num == 2) //오른쪽 앞
+	{
+		T2 = glm::translate(T2, glm::vec3(BLOCK_SIZE / 2 + WHEEL_SIZE / 4, 0.0f, BLOCK_SIZE * 0.5f));
+	}
+	if (num == 3) //왼쪽 뒤
+	{
+		T2 = glm::translate(T2, glm::vec3(-(BLOCK_SIZE / 2 + WHEEL_SIZE / 4), 0.0f, -BLOCK_SIZE * 0.5f));
+	}
+	if (num == 4)  //오른쪽 뒤
+	{
+		T2 = glm::translate(T2, glm::vec3(BLOCK_SIZE / 2 + WHEEL_SIZE / 4, 0.0f, -BLOCK_SIZE * 0.5f));
+	}
+	return T2;
+}
+glm::mat4 Wheel_on_000(int num, int type) //num은 4개 바퀴의 번호, type은 실린더, 뚜껑 객체 종류
 {
 	glm::mat4 T = glm::mat4(1.0f);
 	glm::mat4 Ry = glm::mat4(1.0f);
@@ -147,7 +190,7 @@ glm::mat4 Wheel_on_000(int type)
 		T = glm::translate(T, glm::vec3(0.0f, 0.0f, WHEEL_SIZE / 4));
 	}
 
-	return Ry * T;
+	return Wheels(num) * Ry * T;
 }
 
 float c_dx = 0.0f;
@@ -284,21 +327,21 @@ GLvoid drawScene()
 	}
 	glutSwapBuffers();  // 화면에 출력하기
 }
-void draw_wheels(int modelLoc)
+void draw_wheels(int modelLoc, int num)
 {
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(Wheel_on_000(0)));
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(Wheel_on_000(num, 0)));
 	GLUquadricObj* qobj1;
 	qobj1 = gluNewQuadric();						// 객체 생성하기
 	gluCylinder(qobj1, WHEEL_SIZE, WHEEL_SIZE, WHEEL_SIZE / 2, 20, 8);
 	gluDeleteQuadric(qobj1);
 	// 바퀴 뚜껑1
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(Wheel_on_000(1)));
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(Wheel_on_000(num, 1)));
 	GLUquadricObj* qobj2;
 	qobj2 = gluNewQuadric();
 	gluDisk(qobj2, 0.0f, WHEEL_SIZE, 20, 8);
 	gluDeleteQuadric(qobj2);
 	// 바퀴 뚜껑2
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(Wheel_on_000(2)));
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(Wheel_on_000(num, 2)));
 	GLUquadricObj* qobj3;
 	qobj3 = gluNewQuadric();
 	gluDisk(qobj3, 0.0f, WHEEL_SIZE, 20, 8);
@@ -311,16 +354,21 @@ void drawObjects(int modelLoc, int mod)
 	glBindVertexArray(vao[0]);
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 
-	//// 차체
-	//glBindVertexArray(vao[1]);
-	//glDrawArrays(GL_TRIANGLES, 0, 6 * 6);
-	//glDrawArrays(GL_TRIANGLES, 36, 6 * 6);
+	// 차체
+	glBindVertexArray(vao[1]);
+	glDrawArrays(GL_TRIANGLES, 0, 6 * 6);
+	glDrawArrays(GL_TRIANGLES, 36, 6 * 6);
+	// 헤드라이트
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(Headlights(0)));
+	glDrawArrays(GL_TRIANGLES, 72, 6 * 6);	//왼쪽
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(Headlights(1)));
+	glDrawArrays(GL_TRIANGLES, 108, 6 * 6); //오른쪽
 
 	// 바퀴 Wheel_on_000
-	draw_wheels(modelLoc);	//1
-	draw_wheels(modelLoc);	//2
-	draw_wheels(modelLoc);	//3
-	draw_wheels(modelLoc);	//4
+	draw_wheels(modelLoc, 1);	//1
+	draw_wheels(modelLoc, 2);	//2
+	draw_wheels(modelLoc, 3);	//3
+	draw_wheels(modelLoc, 4);	//4
 }
 
 GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
@@ -364,7 +412,6 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 		}
 		break;
 	}
-
 	//카메라 이동
 	case 'z': case 'Z':
 	{
@@ -390,7 +437,6 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 		}
 		break;
 	}
-	
 	//카메라 공전
 	case 'y': case 'Y':
 	{
@@ -539,139 +585,8 @@ void InitBuffer()
 
 }
 
-void initBox()
-{
-	GLfloat rgb[8][3] = {
-			{1.0f, 0.0f, 0.0f},		
-			{0.0f, 1.0f, 0.0f},		
-			{0.0f, 0.0f, 1.0f},		
-			{1.0f, 1.0f, 0.0f},		
-			{0.0f, 1.0f, 1.0f},		
-			{0.0f, 1.0f, 1.0f},		
-			{0.0f, 1.0f, 1.0f},		
-			{0.0f, 1.0f, 1.0f},		
-	};
-	GLfloat BoxColorData[14 * 3][3] = {
-		// Front face - 2tri
-		{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[0][0], rgb[0][1], rgb[0][2]},
-		{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[0][0], rgb[0][1], rgb[0][2]},
-
-		// Front face - 2tri
-		{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[0][0], rgb[0][1], rgb[0][2]},
-		{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[0][0], rgb[0][1], rgb[0][2]},
-
-		// Back face - 2tri
-		{rgb[4][0], rgb[4][1], rgb[4][2]}, {rgb[5][0], rgb[5][1], rgb[5][2]}, {rgb[6][0], rgb[6][1], rgb[6][2]},
-		{rgb[4][0], rgb[4][1], rgb[4][2]}, {rgb[6][0], rgb[6][1], rgb[6][2]}, {rgb[7][0], rgb[7][1], rgb[7][2]},
-
-		// Left face - 2tri
-		{rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]},
-		{rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]},
-
-		// Right face - 2tri
-		{rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]},
-		{rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]},
-
-		// Top face - 2tri
-		{rgb[2][0], rgb[2][1], rgb[2][2]}, {rgb[2][0], rgb[2][1], rgb[2][2]}, {rgb[2][0], rgb[2][1], rgb[2][2]},
-		{rgb[2][0], rgb[2][1], rgb[2][2]}, {rgb[2][0], rgb[2][1], rgb[2][2]}, {rgb[2][0], rgb[2][1], rgb[2][2]},
-
-		// Bottom face - 2tri
-		{rgb[3][0], rgb[3][1], rgb[3][2]}, {rgb[3][0], rgb[3][1], rgb[3][2]}, {rgb[3][0], rgb[3][1], rgb[3][2]},
-		{rgb[3][0], rgb[3][1], rgb[3][2]}, {rgb[3][0], rgb[3][1], rgb[3][2]}, {rgb[3][0], rgb[3][1], rgb[3][2]}
-	};
-	GLfloat vertices[10][3] = {
-			{-BOX_SIZE, BOX_SIZE, BOX_SIZE},			// Vertex 0 앞, 왼쪽 위
-			{-BOX_SIZE, -BOX_SIZE, BOX_SIZE},			// Vertex 1 앞, 왼쪽 아래
-			{BOX_SIZE, -BOX_SIZE, BOX_SIZE},			// Vertex 2 앞, 오른쪽 아래
-			{BOX_SIZE, BOX_SIZE, BOX_SIZE},			// Vertex 3 앞, 오른쪽 위
-			{-BOX_SIZE, BOX_SIZE, -BOX_SIZE},			// Vertex 4 뒤, 왼쪽 위
-			{-BOX_SIZE, -BOX_SIZE, -BOX_SIZE},			// Vertex 5 뒤, 왼쪽 아래
-			{BOX_SIZE, -BOX_SIZE, -BOX_SIZE},			// Vertex 6 뒤, 오른쪽 아래
-			{BOX_SIZE, BOX_SIZE, -BOX_SIZE},			// Vertex 7 뒤, 오른쪽 위
-
-			{ 0.0f, BOX_SIZE, BOX_SIZE },		// Vertex 8 앞, 가운데 위
-			{ 0.0f, -BOX_SIZE, BOX_SIZE}			// Vertex 9 앞, 가운데 아래
-	};
-	GLfloat BoxFigure[14 * 3][3] = {
-		// 앞면(왼쪽) //019 098 892 823
-		{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[1][0], vertices[1][1], vertices[1][2]}, {vertices[9][0], vertices[9][1], vertices[9][2]},  // 삼각형 1
-		{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[9][0], vertices[9][1], vertices[9][2]}, {vertices[8][0], vertices[8][1], vertices[8][2]},  // 삼각형 2
-
-		// 앞면(오른쪽)
-		{vertices[8][0], vertices[8][1], vertices[8][2]}, {vertices[9][0], vertices[9][1], vertices[9][2]}, {vertices[2][0], vertices[2][1], vertices[2][2]},  // 삼각형 3
-		{vertices[8][0], vertices[8][1], vertices[8][2]}, {vertices[2][0], vertices[2][1], vertices[2][2]}, {vertices[3][0], vertices[3][1], vertices[3][2]},  // 삼각형 4
-
-		// 뒷면
-		{vertices[4][0], vertices[4][1], vertices[4][2]}, {vertices[5][0], vertices[5][1], vertices[5][2]}, {vertices[7][0], vertices[7][1], vertices[7][2]},  // 삼각형 5
-		{vertices[5][0], vertices[5][1], vertices[5][2]}, {vertices[6][0], vertices[6][1], vertices[6][2]}, {vertices[7][0], vertices[7][1], vertices[7][2]},  // 삼각형 6
-
-		// 왼쪽면
-		{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[4][0], vertices[4][1], vertices[4][2]}, {vertices[1][0], vertices[1][1], vertices[1][2]},  // 삼각형 7
-		{vertices[1][0], vertices[1][1], vertices[1][2]}, {vertices[4][0], vertices[4][1], vertices[4][2]}, {vertices[5][0], vertices[5][1], vertices[5][2]},  // 삼각형 8
-
-		// 오른쪽면
-		{vertices[3][0], vertices[3][1], vertices[3][2]}, {vertices[2][0], vertices[2][1], vertices[2][2]}, {vertices[7][0], vertices[7][1], vertices[7][2]},  // 삼각형 9
-		{vertices[2][0], vertices[2][1], vertices[2][2]}, {vertices[6][0], vertices[6][1], vertices[6][2]}, {vertices[7][0], vertices[7][1], vertices[7][2]},  // 삼각형 10
-
-		// 윗면
-		{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[3][0], vertices[3][1], vertices[3][2]}, {vertices[4][0], vertices[4][1], vertices[4][2]},  // 삼각형 11
-		{vertices[4][0], vertices[4][1], vertices[4][2]}, {vertices[3][0], vertices[3][1], vertices[3][2]}, {vertices[7][0], vertices[7][1], vertices[7][2]},  // 삼각형 12
-
-		// 바닥면
-		{vertices[1][0], vertices[1][1], vertices[1][2]}, {vertices[2][0], vertices[2][1], vertices[2][2]}, {vertices[5][0], vertices[5][1], vertices[5][2]},  // 삼각형 13
-		{vertices[5][0], vertices[5][1], vertices[5][2]}, {vertices[2][0], vertices[2][1], vertices[2][2]}, {vertices[6][0], vertices[6][1], vertices[6][2]},  // 삼각형 14
-	};
-	//아래 몸통 
-	for (int j = 0; j < 14 * 3; j++)
-	{
-		for (int k = 0; k < 3; k++)
-		{
-			Box[j][k] = BoxFigure[j][k];
-			Box_Color[j][k] = BoxColorData[j][k];
-		}
-	}
-}
 void initBlock()
 {
-	GLfloat rgb[8][3] = {
-		{1.0f, 1.0f, 1.0f},		// Vertex 0 white
-		{1.0f, 1.0f, 0.0f},		// Vertex 1 110
-		{1.0f, 0.0f, 0.0f},		// Vertex 2 red
-		{1.0f, 0.0f, 1.0f},		// Vertex 3 101
-		{0.0f, 1.0f, 1.0f},		// Vertex 4 011
-		{0.0f, 1.0f, 0.0f},		// Vertex 5 green
-		{0.0f, 0.0f, 0.0f},		// Vertex 6 black
-		{0.0f, 0.0f, 1.0f},		// Vertex 7 blue
-	};
-	GLfloat CubeColorData[1][TRI_COUNT * 3][3] = {
-		{
-			// Front face - 2tri
-			{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[2][0], rgb[2][1], rgb[2][2]},
-			{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[2][0], rgb[2][1], rgb[2][2]}, {rgb[3][0], rgb[3][1], rgb[3][2]},
-
-			// Back face - 2tri
-			{rgb[4][0], rgb[4][1], rgb[4][2]}, {rgb[5][0], rgb[5][1], rgb[5][2]}, {rgb[6][0], rgb[6][1], rgb[6][2]},
-			{rgb[4][0], rgb[4][1], rgb[4][2]}, {rgb[6][0], rgb[6][1], rgb[6][2]}, {rgb[7][0], rgb[7][1], rgb[7][2]},
-
-			// Left face - 2tri
-			{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[4][0], rgb[4][1], rgb[4][2]}, {rgb[7][0], rgb[7][1], rgb[7][2]},
-			{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[7][0], rgb[7][1], rgb[7][2]}, {rgb[3][0], rgb[3][1], rgb[3][2]},
-
-			// Right face - 2tri
-			{rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[5][0], rgb[5][1], rgb[5][2]}, {rgb[6][0], rgb[6][1], rgb[6][2]},
-			{rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[6][0], rgb[6][1], rgb[6][2]}, {rgb[2][0], rgb[2][1], rgb[2][2]},
-
-			// Top face - 2tri
-			{rgb[3][0], rgb[3][1], rgb[3][2]}, {rgb[2][0], rgb[2][1], rgb[2][2]}, {rgb[6][0], rgb[6][1], rgb[6][2]},
-			{rgb[3][0], rgb[3][1], rgb[3][2]}, {rgb[6][0], rgb[6][1], rgb[6][2]}, {rgb[7][0], rgb[7][1], rgb[7][2]},
-
-			// Bottom face - 2tri
-			{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[1][0], rgb[1][1], rgb[1][2]}, {rgb[5][0], rgb[5][1], rgb[5][2]},
-			{rgb[0][0], rgb[0][1], rgb[0][2]}, {rgb[5][0], rgb[5][1], rgb[5][2]}, {rgb[4][0], rgb[4][1], rgb[4][2]}
-		}
-	};
-
 	//아래 몸체
 	if (true)
 	{
@@ -777,6 +692,66 @@ void initBlock()
 			Block_Color[1][j][0] = 0.0f;
 			Block_Color[1][j][1] = 1.0f;
 			Block_Color[1][j][2] = 1.0f;
+		}
+	}
+
+	//헤드라이트 1, 2 (정육면체)
+	if (true)
+	{
+		GLfloat vertices[8][3] = {
+			{-BLOCK_SIZE / 8,	-BLOCK_SIZE / 8,	-BLOCK_SIZE / 8},	// Vertex 0
+			{ BLOCK_SIZE / 8,	-BLOCK_SIZE / 8,	-BLOCK_SIZE / 8},	// Vertex 1
+			{ BLOCK_SIZE / 8,	BLOCK_SIZE	/ 8,		-BLOCK_SIZE / 8},  // Vertex 2
+			{-BLOCK_SIZE / 8,	BLOCK_SIZE	/ 8,		-BLOCK_SIZE / 8},  // Vertex 3
+			{-BLOCK_SIZE / 8,	-BLOCK_SIZE / 8,	BLOCK_SIZE  / 8},	// Vertex 4
+			{ BLOCK_SIZE / 8,	-BLOCK_SIZE / 8,	BLOCK_SIZE  / 8},	// Vertex 5
+			{ BLOCK_SIZE / 8,	BLOCK_SIZE	/ 8,		BLOCK_SIZE  / 8},	// Vertex 6
+			{-BLOCK_SIZE / 8,	BLOCK_SIZE	/ 8,		BLOCK_SIZE  / 8}	// Vertex 7
+		};
+		//큐브 데이터 초기화
+		GLfloat CubeFigure[1][TRI_COUNT * 3][3] = {
+			{
+				// Front face - 2tri	012 023 -02
+				{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[1][0], vertices[1][1], vertices[1][2]}, {vertices[2][0], vertices[2][1], vertices[2][2]},
+				{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[2][0], vertices[2][1], vertices[2][2]}, {vertices[3][0], vertices[3][1], vertices[3][2]},
+
+				// Back face - 2tri		456 467 -46
+				{vertices[4][0], vertices[4][1], vertices[4][2]}, {vertices[5][0], vertices[5][1], vertices[5][2]}, {vertices[6][0], vertices[6][1], vertices[6][2]},
+				{vertices[4][0], vertices[4][1], vertices[4][2]}, {vertices[6][0], vertices[6][1], vertices[6][2]}, {vertices[7][0], vertices[7][1], vertices[7][2]},
+
+				// Left face - 2tri		047 073 -07
+				{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[4][0], vertices[4][1], vertices[4][2]}, {vertices[7][0], vertices[7][1], vertices[7][2]},
+				{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[7][0], vertices[7][1], vertices[7][2]}, {vertices[3][0], vertices[3][1], vertices[3][2]},
+
+				// Right face - 2tri	156 162 -16
+				{vertices[1][0], vertices[1][1], vertices[1][2]}, {vertices[5][0], vertices[5][1], vertices[5][2]}, {vertices[6][0], vertices[6][1], vertices[6][2]},
+				{vertices[1][0], vertices[1][1], vertices[1][2]}, {vertices[6][0], vertices[6][1], vertices[6][2]}, {vertices[2][0], vertices[2][1], vertices[2][2]},
+
+				// Top face - 2tri		326 367 -36
+				{vertices[3][0], vertices[3][1], vertices[3][2]}, {vertices[2][0], vertices[2][1], vertices[2][2]}, {vertices[6][0], vertices[6][1], vertices[6][2]},
+				{vertices[3][0], vertices[3][1], vertices[3][2]}, {vertices[6][0], vertices[6][1], vertices[6][2]}, {vertices[7][0], vertices[7][1], vertices[7][2]},
+
+				// Bottom face - 2tri	015 054 -05
+				{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[1][0], vertices[1][1], vertices[1][2]}, {vertices[5][0], vertices[5][1], vertices[5][2]},
+				{vertices[0][0], vertices[0][1], vertices[0][2]}, {vertices[5][0], vertices[5][1], vertices[5][2]}, {vertices[4][0], vertices[4][1], vertices[4][2]}
+			}
+		};
+
+		for (int j = 0; j < TRI_COUNT * 3; j++)
+		{
+			for (int k = 0; k < 3; k++)
+			{
+				Block[2][j][k] = CubeFigure[0][j][k];
+
+				Block[3][j][k] = CubeFigure[0][j][k];
+			}
+			Block_Color[2][j][0] = 1.0f;
+			Block_Color[2][j][1] = 1.0f;
+			Block_Color[2][j][2] = 0.0f;
+
+			Block_Color[3][j][0] = 1.0f;
+			Block_Color[3][j][1] = 1.0f;
+			Block_Color[3][j][2] = 0.0f;
 		}
 	}
 }
